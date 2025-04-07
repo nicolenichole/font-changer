60247e7d6259194089d9a64733f87f2f64f3492b
nicolenichole <lnicoleu23@gmail.com>
2025-04-07 13:58:42 -0500

    Adding OpenDyslexia Font

--- popup.js
+++ popup.js
@@ -5,11 +5,32 @@ document.getElementById("fontSelect").addEventListener("change", (e) => {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
         func: (font) => {
-          document.body.style.fontFamily = font;
+          const styleId = "open-dyslexic-style";
+          const existingStyle = document.getElementById(styleId);
+  
+          if (font === "OpenDyslexic") {
+            if (!existingStyle) {
+              const style = document.createElement("style");
+              style.id = styleId;
+              style.textContent = `
+                @font-face {
+                  font-family: 'OpenDyslexic';
+                  src: url('${chrome.runtime.getURL("fonts/OpenDyslexic-Regular.otf")}') format('opentype');
+                }
+              `;
+              document.head.appendChild(style);
+            }
+            document.body.style.fontFamily = "'OpenDyslexic', sans-serif";
+          } else {
+            // Remove OpenDyslexic style tag if present
+            if (existingStyle) {
+              existingStyle.remove();
+            }
+            document.body.style.fontFamily = font;
+          }
         },
         args: [font],
       });
     });
 });
-  
   
